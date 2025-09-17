37266a02dda68b53e723b4dd31ccf944d4d4b1d5
Aayush <aayushjha2431@gmail.com>
2025-09-17 21:22:26 +0530

    Started Streamlit and modified the folder structure

---
+++ Monthly/App/app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.title("Aayush Jha")
+st.header("Header")
+st.subheader("Subheader")
+st.markdown("_`hi`_")
+st.caption("Caption")
+st.divider()
+
+
+
+pressed = st.button("Press me")
